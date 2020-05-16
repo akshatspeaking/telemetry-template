e364bb812fddd6261d6ce24a8ca6212b13b25d70
akshatspeaking <akshatspeaking@gmail.com>
2020-05-16 19:21:00 +0530

    corrections

--- script.js
+++ script.js
@@ -25,21 +25,29 @@ document.addEventListener("scroll", (e) => {
 
 document.querySelectorAll(".radiobtn").forEach(x => {
    
-   document.querySelectorAll(".radiobtn").forEach(x => {x.classList.remove("selected")})
+   
    // document.querySelectorAll(".radiobtn").forEach(x => {x.style.border = "1px solid black"})
    
    x.addEventListener("click", (e) => {
-      
+
+      document.querySelectorAll(".radiobtn").forEach(x => {
+         x.classList.remove("selected");
+         x.style.transform = "scale(1)";
+         // |transform-functions|initial|inherit 
+   
+      })
+
+      e.target.style.transform = "scale(1.2)"
       e.target.classList.add("selected");
          if (e.target.classList.contains("rb1")){
-            document.querySelector(".editheading").innerText = "Lorem Ipsum One"
-            document.querySelector(".edittext").innerText = "lorem ipsum lorem ipsum"      
+            document.querySelector(".editheading").innerText = "Lorem ipsum dolor sit magna feugiat etiam sed dolor"
+            document.querySelector(".edittext").innerText = "Maecenas id feugiat nunc. Integer gravida augue libero, quis pellentesque amet pharetra a. Ut sagittis ipsum nec velit porttitor, sed convallis ligula pellentesque. Mauris et dignissim sem lacinia lorem ipsum dolor."      
          } else if (e.target.classList.contains("rb2")){
-            document.querySelector(".editheading").innerText = "Lorem Ipsum Two";
-            document.querySelector(".edittext").innerText = "lorem ipsum lorem ipsum lorem lorem lorem";
+            document.querySelector(".editheading").innerText = "Ipsum lorem accumsan sed nullam feugiat veroeros";
+            document.querySelector(".edittext").innerText = "Veritatis, accusantium, nemo. Officia neque explicabo nihil! Sunt vero reprehenderit, necessitatibus atque, enim autem animi illum quaerat at, corporis dolore ipsam. Voluptatem culpa sapiente sed veroeros.";
          } else if (e.target.classList.contains("rb3")){
-            document.querySelector(".editheading").innerText = "Lorem Ipsum Three";
-            document.querySelector(".edittext").innerText = "lorem ipsum lorem ipsum lorem lorem lorem lorem ipsum lorem ipsum";
+            document.querySelector(".editheading").innerText = "Adipiscing sed impedit volutpat magna cursus lorem";
+            document.querySelector(".edittext").innerText = "Perspiciatis saepe, mollitia itaque magna etiam dolor sit amet. Sed rerum porro sint iure illo accusantium in amet totam, est. Ipsa nobis ad optio impedit quidem inventore libero, aperiam, nesciunt dolor sit amet.";
          }         
       
       
