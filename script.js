11b64c7cda5a1b0790193d3d9fceabbb6f8bd30c
akshatspeaking <akshatspeaking@gmail.com>
2020-05-16 12:53:18 +0530

    updates and corrections

--- script.js
+++ script.js
@@ -5,3 +5,30 @@
 //      document.getElementById("header").style.backgroundColor = "transparent";
 //    }
 //  } 
+
+const hero = document.querySelector(".hero");
+const header = document.querySelector("header");
+const scroll = window.scrollY;
+document.addEventListener("scroll", (e) => {
+
+   let offset = window.scrollY;
+   hero.style.backgroundPositionY = -offset * 0.5 + "px";
+
+   if (scrollY >= 848) {
+      header.style.backgroundColor = "#ff887bdc";
+   }
+   else {
+      header.style.backgroundColor = "transparent";
+   }
+   console.log(scrollY);
+})
+
+document.querySelectorAll(".radiobtn").forEach(x => {
+   x.addEventListener("click", (e) => {
+      document.querySelectorAll(".radiobtn").forEach(x => {x.style.border = "1px solid black"})
+      e.target.style.border = "none";
+      document.querySelector(".editheading").innerText = "Lorem Ipsum One"
+      document.querySelector(".edittext").innerText = "lorem ipsum lorem ipsum"
+      
+   })
+})
